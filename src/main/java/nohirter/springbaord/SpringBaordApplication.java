5f9d34626e1bbfe2b2ae3c69a6aab36f065c2c35
unhappiness404 <unhappiness404@gmail.com>
2024-03-19 15:26:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/nohirter/springbaord/SpringBaordApplication.java
@@ -0,0 +1,13 @@
+package nohirter.springbaord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nohirter/springbaord/SpringBaordApplicationTests.java
@@ -0,0 +1,13 @@
+package nohirter.springbaord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
